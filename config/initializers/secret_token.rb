f92dcc2d44f4714b8df15b5e1235afeead57f46e
Gerhardk <gerhard@platform45.com>
2012-07-31 23:36:38 +0200

    Getting Project Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitItApp::Application.config.secret_token = 'f8eec816919915af9490c6b17139f92d30e5acb02b45d90340e1dcd9021214f5be6ffabaabb2d18c215f72ae38d514c2e871ad7190e87f98a89dc075165d895e'
